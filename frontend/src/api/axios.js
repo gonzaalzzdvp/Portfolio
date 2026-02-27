28281326e53f5d466a1726e70e3a26e7115e5cd8
gonzaalzz <hectorlgonzalez91@gmail.com>
2026-02-27 16:47:12 -0400

    Portfolio deploy v0.0.7

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://portfolio-bcknd-1965.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, 
   xsrfCookieName: "csrftoken",
   xsrfHeaderName: "X-CSRFToken",
